1ab76b04a2a37ee2fb6a7ad97088485d2f547c73
IamNotMere <63594019+IamNotMere@users.noreply.github.com>
2023-12-03 22:49:56 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,113 @@
+import asyncio
+import discord
+from discord.ext import commands
+import sqlite3
+import time
+from keys import T
+
+timetillfull = 0
+max_resin_time = 0
+response = None
+data = None
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def sresin(ctx):
+    global response, timetillfull, max_resin_time
+    await ctx.send("Please provide your input. You have 10 seconds to respond.")
+
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+
+    try:
+        response_message = await bot.wait_for('message', check=check, timeout=10)
+        response = response_message.content
+        await ctx.send('Your input is: ' + response)
+
+        if response is not None:
+            conn = sqlite3.connect("discord.db")
+            cursor = conn.cursor()
+
+            # Create a Table
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS users (
+                    Resin INTEGER NOT NULL
+                )
+            ''')
+            conn.commit()
+
+            # Insert Data
+            cursor.execute('''
+                INSERT INTO users (Resin) VALUES (?)''', (response,))
+            cursor.execute('''SELECT Resin FROM users''')
+            data = cursor.fetchone()
+
+            if data is not None and int(data[0]) <= 160:
+                resintime = 0
+                resintillfull = 160 - int(data[0])
+                timetillfull = resintillfull * 8 * 60
+                resintime += timetillfull
+                max_resin_time = time.time() + resintime
+
+    except asyncio.TimeoutError:
+        await ctx.send('You did not respond in time.')
+
+@bot.command()
+async def genshin(ctx):
+    global timetillfull, max_resin_time
+    if timetillfull >= 76800:
+        embed_description = 'Your resin is currently capped'
+    else:
+        embed_description = f'Your resin will be capped <t:{round(max_resin_time)}:R>'
+
+    embed = discord.Embed(
+        title='Genshin Resin',
+        url='https://google.com',
+        description=embed_description,
+        color=0x440d5c
+    )
+    embed.set_author(
+        name='Genshin Impact',
+        icon_url='https://static.wikia.nocookie.net/gensin-impact/images/8/80/Genshin_Impact.png/revision/latest?cb=20230121174225'
+    )
+    await ctx.send(embed=embed)
+@bot.command()
+async def bday(ctx):
+    await ctx.send('Happy Birthday')
+
+
+@bot.command()
+async def birthday(ctx):
+    await ctx.send(
+        'Contained Personal info for personal use so changed.')
+
+
+@bot.command()
+async def mere(ctx):
+    await ctx.send('''He is my creator''')
+
+
+@bot.command()
+async def command(ctx):
+    await ctx.send('''!bday - birthday wishes
+!birthday - best birthday wishes
+!mere - info about mere
+!genshin - check when your resin will cap.(Only use after using !sresin)
+!sresin - Input how much resin you currently have''')
+
+
+@bot.command()
+async def explosion(ctx):
+    await ctx.send('Personal use information so i changed it')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is operational')
+
+
+def run_discord_bot():
+    bot.run(T().TOKEN1())
---
+++ keys.py
@@ -0,0 +1,5 @@
+class T:
+    def TOKEN1(self):
+      return 'Your Token'
+    def TOKEN2(self):
+      return 'Your Token'
